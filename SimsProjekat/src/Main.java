599a322f6e57cd75e5d59576bdb9b31c4a09ff85
SaraSukovic <sarasukovic27@gmail.com>
2017-06-28 15:23:30 +0200

    prvi commit

--- SimsProjekat/src/Main.java
+++ SimsProjekat/src/Main.java
@@ -1,15 +1,11 @@
 
+
 public class Main {
     public static void main(String[] args) {
     	//lala
-    	System.out.println("SIMS");
-    	System.out.println("cao saro");
-    	System.out.println("opet");
-    	System.out.println("cao boksi");
-    	System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaa");
-    	//aaaaA
-    	System.out.println("kkkkk");
-    	System.out.println("AJDE");
+    	
+    	Editor e = new Editor();
+    	e.show();
     }
 	
 	
