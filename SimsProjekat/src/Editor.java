ab781b181e0297c3e49240582d4625aa83af6b69
SaraSukovic <sarasukovic27@gmail.com>
2017-06-28 15:57:32 +0200

    MenuBar osnovni

---
+++ SimsProjekat/src/Editor.java
@@ -0,0 +1,46 @@
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.*;
+
+
+public class Editor extends JFrame {
+    private MenuBar menu;	
+	
+	
+
+	
+	public Editor() {
+		setBounds(0, 0, 800, 600);
+		setMinimumSize(new Dimension(400, 300));
+		setLocationRelativeTo(null);
+		setTitle("ADMS Editor");
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				if(confirm("Are you sure you want to close ?"))
+					System.exit(0);
+			}
+		});
+		
+
+		menu = new MenuBar(this);
+		setJMenuBar(menu);
+		setVisible(true);
+	}
+	
+	
+	public boolean confirm(String message, String title) {
+		return JOptionPane.OK_OPTION == JOptionPane.showConfirmDialog(this, message, title, 
+										JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+	}
+	
+	
+	public boolean confirm(String message) {
+		return confirm(message, null);
+	}
+	
+	
+}
--- SimsProjekat/src/Main.java
+++ SimsProjekat/src/Main.java
@@ -1,11 +1,12 @@
 
 
 public class Main {
-    public static void main(String[] args) {
+    
+	public static void main(String[] args) {
     	//lala
     	
     	Editor e = new Editor();
-    	e.show();
+    
     }
 	
 	
